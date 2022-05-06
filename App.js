f5dc17e251629d97726f8fea9f39b19ca2306535
Abdiel Jefte Santillan Luna <54774942+Abdielsantillan@users.noreply.github.com>
2022-05-05 20:50:21 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import MainStack from './navigation/mainStack';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      
+        <SafeAreaView style={{ flex:1 }}>
+           <MainStack/>
+        </SafeAreaView>
+    );
+  }
+}
+
+
+export default App;
---
+++ todo.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native';
+import ScreenRegistro from './screens/screenRegistro';
+
+AppRegistry.registerComponent('Bocadillo', () => ScreenRegistro);
+
